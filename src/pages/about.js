f20c6f7b993acc328908e6b814f7ce20a5b67b30
Matteo Melani <m@ellipsis.ai>
2019-07-14 20:47:07 -0700

    Add a bunch of pages for fun

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+import Layout from "../components/layout";
+
+export default () => (
+    <Layout>
+        <Header headerText="About Gatsby" /> 
+        About me
+    </Layout>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <Layout> 
+        <p>Hello World </p>
+        <p><img src="https://source.unsplash.com/random/400x200" alt="" /></p>
+    </Layout> 
+)
