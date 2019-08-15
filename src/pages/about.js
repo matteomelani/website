152992ed825ccab6881b9431648ce304f5bb56f6
Matteo Melani <m@ellipsis.ai>
2019-08-15 16:59:39 -0700

    Remove bad text from about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,8 @@ import SEO from "../components/seo"
 export default () => (
     <Layout breadcrumbsItems={[{ text:'Matteo Melani', link: '/' },{ text:'About', link: '/about' }]}>
       <SEO title = 'About' description = 'About Matteo Melani.' pathname = '/about' zType = 'profile' />
-      <p>My name is Matteo Melani. I am an entrepreneur in Silicon Valley. I solved problems at LBNL, SLAC, Microsoft and Twitter. I enjoy writting code as much as deleting it.</p>
-      <p>I love bicyles and riding them. When possible, for long distances and across mountains.</p>
+      <p>Lorem ipsus...</p>
+      <p>Bingo bongo</p>
       <div id="contact-info">
         <p>
           <a href="https://www.twitter.com/matteomelani">@matteomelani</a>
