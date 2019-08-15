f21ada4e6b58e8be782ab98365bd8c5ba06b54c7
Matteo Melani <m@ellipsis.ai>
2019-08-15 16:39:01 -0700

    Tricking

--- src/components/seo12.js
+++ src/components/seo12.js
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-
+// Pippo
 const SEO = ({ title, description, image, pathname, zType }) => (
     <StaticQuery
         query={query}
