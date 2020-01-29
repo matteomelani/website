94bcf38a9d2d0e60b89704cc9183af6e52898cc5
Matteo Melani <m@ellipsis.ai>
2020-01-29 15:17:19 -0800

    trying different import statemenet to debug build

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,8 +1,8 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import PostItem from "../components/postitem"
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import PostItem from '../components/PostItem'
 
 
 export default ({ data }) => (
